d32eb01c1476307fb5522d717226578aeb80b5c9
Alan Hamlett <alan.hamlett@gmail.com>
2018-06-06 21:28:15 -0700

    update csv formatter to new data dump schema

--- wakadump/formats/csv.py
+++ wakadump/formats/csv.py
@@ -31,77 +31,95 @@ class Formatter(object):
             with click.progressbar(self.data['days'],
                                 label='Exporting to CSV file...',
                                 fill_char=click.style('#', fg='blue')) as days:
+
                 for day in days:
                     data = [
                         day['date'],
                         day['grand_total']['total_seconds'],
                     ]
+                    entities = self._add_data_for_columns(
+                        data,
+                        self.projects,
+                        day['projects'],
+                        is_project=True,
+                    )
                     self._add_data_for_columns(data,
-                                                self.projects,
-                                                day['projects'])
+                                               self.languages,
+                                               day['languages'])
                     self._add_data_for_columns(data,
-                                                self.entities,
-                                                day['entities'])
+                                               self.editors,
+                                               day['editors'])
                     self._add_data_for_columns(data,
-                                                self.languages,
-                                                day['languages'])
+                                               self.operating_systems,
+                                               day['operating_systems'])
                     self._add_data_for_columns(data,
-                                                self.editors,
-                                                day['editors'])
+                                               self.categories,
+                                               day['categories'])
                     self._add_data_for_columns(data,
-                                                self.operating_systems,
-                                                day['operating_systems'])
+                                               self.entities,
+                                               entities)
                     w.writerow(data)
 
     def _get_column_names(self):
         self.projects = {}
-        self.entities = {}
         self.languages = {}
         self.editors = {}
         self.operating_systems = {}
+        self.categories = {}
+        self.entities = {}
 
         with click.progressbar(self.data['days'],
                                label='Getting column names...',
                                fill_char=click.style('#', fg='blue')) as days:
             for day in days:
-                self._find_columns(self.projects, day['projects'])
-                self._find_columns(self.entities, day['entities'])
+                self._find_columns(self.projects, day['projects'],
+                                   is_project=True)
                 self._find_columns(self.languages, day['languages'])
                 self._find_columns(self.editors, day['editors'])
                 self._find_columns(self.operating_systems,
                                    day['operating_systems'])
+                self._find_columns(self.categories, day['categories'])
 
         self.projects = sorted(self.projects.keys())
-        self.entities = sorted(self.entities.keys())
         self.languages = sorted(self.languages.keys())
         self.editors = sorted(self.editors.keys())
         self.operating_systems = sorted(self.operating_systems.keys())
+        self.categories = sorted(self.categories.keys())
+        self.entities = sorted(self.entities.keys())
 
         self.columns = [
             'DATE',
             'TOTAL LOGGED SECONDS',
         ]
         self._add_columns('PROJECTS', self.projects)
-        self._add_columns('FILES', self.entities)
         self._add_columns('LANGUAGES', self.languages)
         self._add_columns('EDITORS', self.editors)
         self._add_columns('OPERATING SYSTEMS', self.operating_systems)
+        self._add_columns('CATEGORIES', self.categories)
+        self._add_columns('FILES', self.entities)
 
-    def _find_columns(self, existing, items):
+    def _find_columns(self, existing, items, is_project=False):
         for item in items:
             if item['name'] not in existing:
                 existing[item['name']] = True
+            if is_project:
+                self._find_columns(self.entities, item['entities'])
 
     def _add_columns(self, name, data):
         self.columns.append(name)
         self.columns.extend(data)
 
-    def _add_data_for_columns(self, existing_data, column_names, column_data):
+    def _add_data_for_columns(self, existing_data, column_names, column_data,
+                              is_project=False):
+        entities = []
         data = {}
         for item in column_data:
             data[item['name']] = item.get('total_seconds',
                                           item.get('grand_total',
                                                    {}).get('total_seconds'))
+            if is_project:
+                entities.extend(item['entities'])
         existing_data.append('')
         for name in column_names:
             existing_data.append(data.get(name, 0))
+        return entities
